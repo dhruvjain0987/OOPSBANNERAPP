5fa37564e8ca39b2addc1f718a2c4835f59c2912
dhruvjain0987 <jaindhruv1306@gmail.com>
2026-03-02 09:16:55 +0530

    added feature/uc1-printoops

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -2,7 +2,7 @@ package src;
 
 public class OopsBannerApp {
     public static void main(String args[]){
-        System.out.println("hello");
+        System.out.println("oops");
     }
     
     
